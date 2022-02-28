4db0ca2f8d445e6c642c1bcf305f7a129034eddd
Jakob Geiger <jakobgeiger15@gmail.com>
2022-02-28 17:58:20 -0500

    commit tree 723d4f9920abd9066f1671cdbbf0dea9cf8a7000

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+
+//const year = record.find(superbowlWin).year;
+
+function superbowlWin(record) {
+    const result = record.find(obj => obj.result === "W");
+    return (result ? result.year : undefined);
+}
+
+console.log(year);
+
+//const superbowlWin = record.find(result === "W");
+//console.log(year)
